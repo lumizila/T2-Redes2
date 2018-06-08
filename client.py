8fd37576e23945d7b376b5d294082d3ab12048bf
Luiza Wille <lmwc14@inf.ufpr.br>
2018-06-08 19:11:13 -0300

    programa comentado bonitinho
    Signed-off-by: Luiza Wille <lmwc14@inf.ufpr.br>

--- client.py
+++ client.py
@@ -1,54 +1,67 @@
 import socket
 import struct
 import sys
+from datetime import datetime, date
+from time import sleep
 
+# Define o endereco do grupo multicast
 multicast_group = ('224.3.29.71', 10000)
 
-# Create the datagram socket
+# Cria um socket para se comunicar com o grupo
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Set a timeout so the socket does not block indefinitely when trying
-# to receive data.
+# Define um timeout para que o socket nao seja bloqueado
+# indefinidamente ao tentar receber dados
 sock.settimeout(0.2)
 
-# Set the time-to-live for messages to 1 so they do not go past the
-# local network segment.
+# Define o time to live das mensagens do cliente 
 ttl = struct.pack('b', 1)
+# Define outras opcoes do socket
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
+# String usada para armazenar o input do usuario
 message = "start"
+
+# Enquanto a mensagem nao eh 'quit', o programa continua a enviar
+# operacoes aritmeticas ao grupo multicast
 while(message != "quit"):
     print("-----------------------------//--------------------------------")
     print("Welcome to the calculator Client! Type 'quit' to quit the program.")
     print("To make a calculation, type a reverse polish math expression,\nfor example 5+3 becomes 5 3 +")
     print("-----------------------------//--------------------------------") 
+    # Le input do usuario
     message = input("Please write your expression(or quit): ")
     if(message != "quit"):
-        message = "CM" + message
-        print(message)
+        # testa se o input foi vazio
         if(message == ""):
-            print("ERROR: The message can not be empty.")
+            print(str(datetime.now().time())+" Client "+socket.gethostbyname(socket.gethostname())+" says: ERROR: The message can not be empty.")
+        # Se o input do usuario nao foi vazio, envia ao servidor
         else:  
-            try:
-                # Send data to the multicast group
+                # Adiciona prefixo CM a mensagem para que o servidor saiba 
+                # que esta eh uma mensagem que vem do cliente
+                message = "CM" + message
+                
+                # Envia mensagem ao grupo multicast
                 sent = sock.sendto(message.encode(), multicast_group)
-                print('Sending math expression: ', message)
+                print(str(datetime.now().time())+" Client "+socket.gethostbyname(socket.gethostname())+" says: Sending math expression: ", message)
 
-                # Look for responses from all recipients
+                # Espera por respostas de todos os recebedores
                 while True:
-                    print('Waiting to receive answer')
+                    print(str(datetime.now().time())+" Client "+socket.gethostbyname(socket.gethostname())+" says: Waiting to receive answer")
+                    
                     try:
+                        # Tenta receber mensagem de resposta
                         data, server = sock.recvfrom(64)
                     except socket.timeout:
-                        print('ERROR: timed out, no more responses')
+                        # Caso nao tenha recebido nada ate o tempo do timeout, avisa timeout
+                        print(str(datetime.now().time())+" Client "+socket.gethostbyname(socket.gethostname())+" says: ERROR: timed out, no more responses")
                         break
                     else:
-                        print('Received message: "', data.decode(
-                            'utf-8'), '" from: ', server)
-                        print("RESULT:\n"+message[2:]+" = "+data.decode('utf-8'))
+                        # Caso tenha recebido uma resposta, imprime resposta e o remetente
+                        print(str(datetime.now().time())+" Client "+str(socket.gethostbyname(socket.gethostname()))+" says: Received answer from server: "+ str(server))
+                        print("Resulting answer of the server from the math request: \n"+message[2:]+" = "+data.decode('utf-8'))
                         break
-            except:
-                print("Could not send message through socket")
-                pass
-print('Closing client socket')
+
+print(str(datetime.now().time())+" Client "+socket.gethostbyname(socket.gethostname())+" says: Closing client socket.")
+# Termina/fecha o socket
 sock.close()
--- server.py
+++ server.py
@@ -4,9 +4,13 @@ import struct
 import sys
 import threading
 from time import sleep
+# Biblioteca usada para detecar o tempo
 from datetime import datetime, date
+# Biblioteca usada para a tabela de servidores, 
+# pois deve estar sempre ordenada pelos enderecos IP
 from collections import OrderedDict
 
+#Definindo as operacoes matematicas permitidas
 ops = {
   "+": (lambda a, b: a + b),
   "-": (lambda a, b: a - b),
@@ -14,6 +18,7 @@ ops = {
   "/": (lambda a, b: a / b)
 }
 
+#Funcao que avalia a expressao matematica em formato RPN
 def eval(expression):
   tokens = expression.split()
   stack = []
@@ -37,125 +42,146 @@ def eval(expression):
 
   return stack.pop()
 
+#Funcao usada pelo servidor para enviar mensagem de heartbeat ao grupo multicast
 def send_hearbeat(sockhp):
 
-    print("Heartbeat started")
+    print(str(datetime.now().time())+" Server "+socket.gethostbyname(socket.gethostname())+" says: Heartbeat started.")
     message = 'SM-Heartbeat'
 
     multicast_group1 = ('224.3.29.71', 10000)
 
     while True:
         try:
-           # Send data to the multicast group
+           #Enviando o heartbeat ao grupo multicast
+            print(str(datetime.now().time())+" Server "+socket.gethostbyname(socket.gethostname())+" says: Sending heartbeat.")
             sent = sockhp.sendto(message.encode(), multicast_group1)
 
         except:
-            print("ERROR: Could not send heartbeat.")
+            print(str(datetime.now().time())+" Server "+socket.gethostbyname(socket.gethostname())+" says: ERROR: Could not send heartbeat.")
             pass
-
+	#O servidor espera 10 segundos antes de enviar o proximo heartbeat
         sleep(10)
 
-
+#Funcao que checa quais servidores ainda estao ativos baseando-se 
+#no ultimo hearbeat recebido de cada servidor do grupo 
 def is_active(tabelaServidores):
     timenow = datetime.now().time()
+    print(str(datetime.now().time())+" Server "+socket.gethostbyname(socket.gethostname())+" says: Checking if some server should be removed from the table.") 
     keys = []
+    #Para cada servidor em tabelaServidores, checa quais estao inativos 
+    #e salva seus IPs no array keys
     for key in tabelaServidores:
         timedelta = datetime.combine(
             date.today(), timenow) - datetime.combine(date.today(), tabelaServidores[key])
-        print(key + " last update was: " +
-              str(timedelta.seconds) + " seconds ago")
+        print(str(datetime.now().time())+" Server "+socket.gethostbyname(socket.gethostname())+" says: Server with IP "+ key + " last heartbeat was: " +
+              str(timedelta.seconds) + " seconds ago.")
         if(timedelta.total_seconds() > 40):
             keys.append(key)
         else:
             print(key + " still able to run")
-    #This for is necessary cause, when you delete during a interaction  causes:
-    #RuntimeError: OrderedDict mutated during iteration
+    #Para cada servidor anteriormente salvo em keys, deleta ele da tabelaServidores
     for key in keys:
-        print("Removing " + key + " from table")
+        print(str(datetime.now().time())+" Server "+socket.gethostbyname(socket.gethostname())+" says: Removing " + key + " from table, because it has been 40 seconds I did not receive a heartbeat from it.")
         del tabelaServidores[key]
 
     return tabelaServidores
 
+#Funcao usada pelo servidor quando ele recebe um heartbeat do grupo 
+#multicast, ela serve para atualizar o horario do ultimo heartbeat
+#recebido e para adicionar um novo servidor na tabelaServidores caso
+#este tenha sido o primeiro heartbeat recebido de um determinado servidor. 
 def update_heartbeat(address, tabelaServidores):
     if(str(address) in tabelaServidores):
-        print("Updating Heartbeat table from: " +
+        print(str(datetime.now().time())+" Server "+socket.gethostbyname(socket.gethostname())+" says: Updating Heartbeat table of server: " +
               str(address) + " at " + str(datetime.now().time()))
         tabelaServidores.update({str(address): datetime.now().time()})
     else:
-        print("Adding new server to table: ", address)
+        print(str(datetime.now().time())+" Server "+socket.gethostbyname(socket.gethostname())+" says: Adding new server to table: ", address)
         tabelaServidores.update({str(address): datetime.now().time()})
         tabelaServidores = OrderedDict(
             sorted(tabelaServidores.items(), key=lambda x: x[0]))
-    print("Calling election")
     return tabelaServidores
 
+#Programa principal
 def main():
-    print("-------------");
-    print(eval("3 5 - *"));
-    print("-------------");
-    print(eval("3 * 5 -"));
-    print("-------------");
-    print(eval("3 -5 *"));
-    print("-------------");
-    print(eval("3 5 - 2 *"));
 
-
-    # Create the datagram socket
+    # Cria um socket exclusivo para envio de heartbeat para o grupo multicast
     sockhp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Set the time-to-live for messages to 1 so they do not go past the
-    # local network segment.
+    # Define time-to-live 
     ttl = struct.pack('b', 2)
+
+    # Define as opcoes do socket de heartbeat
     sockhp.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-    # iniciando tabela de servidores
+    # Iniciando tabela de servidores e ja inclui este servidor na tabela
     tabelaServidores = OrderedDict(
         {socket.gethostbyname(socket.gethostname()): datetime.now().time()})
-    print(tabelaServidores.items())
+    print(str(datetime.now().time())+" Server "+str(socket.gethostbyname(socket.gethostname()))+" created tabelaServidores, and included itself in it.")
+    # Inicia a thread que enviara os heartbeats via o socket 'sockhp'
     hb = threading.Thread(target=send_hearbeat, args=(sockhp,))
     hb.start()
 
-    #### SERVER ####
+    #### SERVIDOR ####
+    # Define o endereco do grupo multicast
     multicast_group = '224.3.29.71'
     server_address = ('', 10000)
 
-    # Create the socket
+    # Cria o socket que recebera as mensagens do grupo multicast
+    # e que sera usado para comunicacao com o cliente
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # Bind to the server address
+    # Operacao de bind
     sock.bind(server_address)
 
-    # Tell the operating system to add the socket to the multicast group
-    # on all interfaces.
+    # Avisa o sistema operacional para adicionar o socket ao grupo 
+    # multicast em todas as interfaces
     group = socket.inet_aton(multicast_group)
     mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+    # Define outras opcoes do socket
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-    # Receive/respond loop
+
+    # Loop de recebimento de mensagens e envio de respostas
     while True:
-        print('Server '+socket.gethostbyname(socket.gethostname())+' says: Waiting to receive message')
+        print(str(datetime.now().time())+' Server '+socket.gethostbyname(socket.gethostname())+' says: Waiting to receive message')
+        # Aguarda recebimento de mensagem
         data, address = sock.recvfrom(1024)
-        print('Server '+socket.gethostbyname(socket.gethostname())+' says: Received message: ' + data.decode('utf-8')[2:] + ' from ' + str(address))
+        print(str(datetime.now().time())+' Server '+socket.gethostbyname(socket.gethostname())+' says: Received message: ' + data.decode('utf-8')[2:] + ' from ' + str(address))
+        # Se a mensagem comeca com um 'SM', ela eh uma mensagem que vem de servidor
+        # E portanto eh um heartbeat, a tabela de servidores deve ser atualizada
         if(data[:2].decode('utf-8') == 'SM'):
             tabelaServidores = update_heartbeat(address[0], tabelaServidores)
+        # Se a mensagem comeca com um 'CM' ela eh uma mensagem que vem de um cliente
+        # E portanto, deve ser uma mensagem de operacao matematica
         elif(data[:2].decode('utf-8') == 'CM'):
+            # Checa se algum servidor esta inativo 
             tabelaServidores = is_active(tabelaServidores)
+            # Checa se este servidor eh o primeiro da tabelaServidores, 
+            # se sim, faz operacao matematica e responde o cliente
             if(socket.gethostbyname(socket.gethostname()) == list(tabelaServidores.items())[0][0]):
-                print("Server "+str(socket.gethostbyname(socket.gethostname()))+" received expression: " + str(data[2:].decode('utf-8')))
+                print(str(datetime.now().time())+" Server "+str(socket.gethostbyname(socket.gethostname()))+" says: message from client, it is the expression: " + str(data[2:].decode('utf-8')))
+                # Realiza operacao matematica
                 result = eval(data[2:].decode('utf-8'))
-                print("Server "+socket.gethostbyname(socket.gethostname())+" got result: " + str(result))
-                print("Server "+str(socket.gethostbyname(socket.gethostname()))+" is sending result to "+ str(address))
+                print(str(datetime.now().time())+" Server "+socket.gethostbyname(socket.gethostname())+" got result: " + str(result))
+                print(str(datetime.now().time())+" Server "+str(socket.gethostbyname(socket.gethostname()))+" is sending result to "+ str(address))
+                # Responde o cliente com o resultado da operacao 
                 sock.sendto(str(result).encode(), address)
+            # Se este servidor nao eh o primeiro da tabela, ele nao eh o lider
             else:
-                print("Server "+socket.gethostbyname(socket.gethostname())+" says: I am not the chosen one")
+                print(str(datetime.now().time())+" Server "+socket.gethostbyname(socket.gethostname())+" says: I am not the chosen one")
+        # A mensagem que chegou esta em um formato nao previsto
+        # Servidor responde com mensagem de erro
         else:
-            print("Message not recognized, answering with error message.")
+            print(str(datetime.now().time())+" Server "+socket.gethostbyname(socket.gethostname())+" says: Message not recognized, answering with error message.")
             sock.sendto(("ERROR: Message not recognized by the server.").encode(), address)
-	
+
+    # Junta as threads	
     hb.join()
+    # Fecha os sockets
     print('closing Heartbeat')
     sockhp.close()
-
     print('closing socket')
     sock.close()
 
+# Chama a funcao main
 if __name__ == "__main__":
     main()
